53f0e1f66b76b9501648365b47486ecd7c659681
Jakub Soustal <jakubsoustal@gmail.com>
2021-01-13 15:58:02 +0100

    Tried to edit files and to commit + push.

--- scriptBranch.js
+++ scriptBranch.js
@@ -1,1 +1,8 @@
-asdasdsdqw
+console.log("Does this work ?");
+
+new  function printFileName (localFileName) {
+    let name = localFileName;
+    console.log(name);
+}
+
+printFileName("index.html");
